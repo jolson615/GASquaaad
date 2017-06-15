59cd404e0fec9395975e48a6274e957feaef5704
Clea <cramos@greenwichacademy.org>
2017-06-15 15:45:56 +0000

    done

---
+++ clea.rb
@@ -0,0 +1,9 @@
+clea = {
+     :name => "Clea",
+    :last_name => "Ramos",
+    :age => 14,
+    :favorite_foods => ["mashed potatoes","mangoes","chocolate"],
+    :eye_color => "brown",
+    :hair_color => "black",
+    :bilingual => true
+}

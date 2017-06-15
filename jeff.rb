c4eb0e73a37d6dca8f09502a2b32628269bf96fb
Jeffrey L Olson Jr <jolson@Jeffreys-MacBook-Pro.local>
2017-06-15 09:21:02 -0400

    initial commit

---
+++ jeff.rb
@@ -0,0 +1,5 @@
+jeff = {
+  :name => "Jeff",
+  :age => 28,
+  :favorite_shows => ["Bravest Warriors", "Adventure Time", "Steven Universe"]
+}
---
+++ roster.rb
@@ -0,0 +1,1 @@
+students=[ "Adrian", "Anya", "William", "Elizabeth", "Olivia"]
